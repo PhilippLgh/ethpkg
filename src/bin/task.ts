96d0b8e7f5abf88c6a69ecbd56107940bfe53bff
philipplgh <philipp@ethereum.org>
2019-11-13 22:45:34 +0100

    improve task runner

--- src/bin/task.ts
+++ src/bin/task.ts
@@ -17,6 +17,47 @@ export const startTask = (name: string) => {
   spinner.text = chalk.white.bgBlack.bold(' ' + name + '  ')
   // @ts-ignore
   spinner.t_org = name
+  return {
+    taskName: name,
+    updateText: (text : string) => spinner.text = text,
+    succeed: (text?: string) => spinner.succeed(chalk.bold(text || name))
+  }
+}
+
+export interface FormatOptions {
+  taskName: string,
+  timeMs : number
+}
+export type FormatCallback = (info : FormatOptions) => string
+
+export const startNewTask = (name: string) => {
+  let _spinner = ora({
+    spinner: {
+      "interval": 100,
+      "frames": [
+        "▓",
+        "▒",
+        "░"
+      ]
+    }
+  })
+  _spinner.start()
+  _spinner.text = chalk.white.bgBlack.bold(' ' + name + '  ')
+  const start = new Date().getTime()
+  let stop = new Date().getTime()
+  const time = () => (stop - start) 
+  return {
+    taskName: name,
+    updateText: (text : string) => _spinner.text = text,
+    succeed: (text?: string | FormatCallback) => {
+      stop = new Date().getTime()
+      if (typeof text === 'function') {
+        text = text({ taskName: name, timeMs: time() })
+      }
+      _spinner.succeed(chalk.bold(text || `${name}\t\t || Time: ${time()} ms ||`))
+    },
+    time
+  }
 }
 
 export const succeed = (msg?: string) => {
