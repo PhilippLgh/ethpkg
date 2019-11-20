8c89f94425e99b2a2ef9708b2ddf3411901b17fb
PhilippLgh <philipp@ethereum.org>
2019-11-20 18:37:07 +0100

    fix: change exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import fs from 'fs'
 import { IPackage, IPackageEntry } from './PackageManager/IPackage'
 
+import PackageManager from './PackageManager/PackageManager'
+export { PackageManager }
+export default new PackageManager()
+
 import * as util from './util'
-import { default as pkg } from './PackageManager/PackageManager'
+export {util as util}
 
 export {default as pkgsign} from './PackageSigner'
 export {default as cert} from './cert'
-export {util as util}
 
-export {pkg as ethpkg}
 
 export { IPackage as IPackage, IPackageEntry as IPackageEntry } from './PackageManager/IPackage'
