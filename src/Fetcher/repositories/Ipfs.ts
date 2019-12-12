6b763bba0657b643c66fac59daa459713b006423
PhilippLgh <philipp@ethereum.org>
2019-12-12 10:30:55 +0100

    feat: add ipfs skeleton

---
+++ src/Fetcher/repositories/Ipfs.ts
@@ -0,0 +1,30 @@
+import { IRepository, IRelease, FetchOptions } from "../IRepository"
+import { extractVersionFromString, extractChannelFromVersionString, versionToDisplayVersion } from "../../utils/FilenameHeuristics"
+import { datestring } from "../../Utils/PackageUtils"
+
+export default class IpfsRepository implements IRepository {
+
+  name: string = 'IpfsRepository'
+
+  private owner: string;
+  private repo: string;
+
+  constructor({ 
+    owner = '', 
+    project = '' 
+  }) {
+    this.owner = owner
+    this.repo = project
+    this.toRelease = this.toRelease.bind(this)
+  }
+
+  private toRelease(releaseInfo : any) : IRelease[] {
+    const releases : any = []
+    return releases
+  }
+  
+  async listReleases(options? : FetchOptions): Promise<IRelease[]> {
+    return []
+  }
+
+}
