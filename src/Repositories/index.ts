6663452288656b0eeaf0a52d71d4ac362fe658c7
PhilippLgh <philipp@ethereum.org>
2019-12-12 13:08:52 +0100

    feat: register ipfs repo

--- src/Repositories/index.ts
+++ src/Repositories/index.ts
@@ -2,23 +2,25 @@ import GitHubRepository from "./GitHub"
 import AzureRepository from "./Azure"
 import NpmRepository from "./Npm"
 import BintrayRepository from "./Bintray"
+import IpfsRepository from "./Ipfs"
 
 import { IRepository } from "./IRepository"
 
 type RepositoryMap = { [index: string] : Function /* constructor of IRepository */ }
 
-export const repos : RepositoryMap = {
+const repos : RepositoryMap = {
   'github': GitHubRepository,
   'azure': AzureRepository,
   'npm': NpmRepository,
-  'bintray': BintrayRepository
+  'bintray': BintrayRepository,
+  'ipfs': IpfsRepository
 }
 
 const getRepository = (str: string, options: any) : IRepository | undefined => {
   if (str in repos) {
     // FIXME find better solution for typed constructors
     // @ts-ignore
-    return new repos[str](options) as IRepository
+    return new repos[str.toLowerCase()](options) as IRepository
   }
   return undefined
 }
