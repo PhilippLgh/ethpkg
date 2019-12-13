8f932c71d5cad1eb1be55a31fadcd7ed4b5370a1
PhilippLgh <philipp@ethereum.org>
2019-12-13 19:53:48 +0100

    chore: small ipfs improvements and experiments

--- src/Repositories/Ipfs.ts
+++ src/Repositories/Ipfs.ts
@@ -1,7 +1,7 @@
 import { IRepository, IRelease, FetchOptions } from "./IRepository"
 import { extractVersionFromString, extractChannelFromVersionString, versionToDisplayVersion } from "../utils/FilenameHeuristics"
 import { datestring } from "../Utils/PackageUtils"
-import { request, downloadStreamToBuffer } from "../Downloader";
+import { request, downloadStreamToBuffer, downloadJson, download } from "../Downloader";
 import { IPackage } from ".."
 
 export default class IpfsRepository implements IRepository {
@@ -20,23 +20,46 @@ export default class IpfsRepository implements IRepository {
     this.toRelease = this.toRelease.bind(this)
   }
 
+  get api() {
+    return 'https://ipfs.infura.io:5001/api/v0'
+  }
+
   private toRelease(releaseInfo : any) : IRelease[] {
     const releases : any = []
     return releases
   }
   
   async listReleases(options? : FetchOptions): Promise<IRelease[]> {
+    const dirHash = 'QmWEV1pjbpKHskbC1hWCu5APNsPPk7XUAtzJEiq6EpCoov'
+    const endpoint = `${this.api}/dag/get?arg=${dirHash}`
+    const packageInfo = await download(endpoint)
+    console.log('package info', packageInfo.toString())
     return []
   }
 
   async publish(pkg: IPackage) {
-    const response = await request('POST', 'https://ipfs.infura.io:5001/api/v0/add?pin=false', {
+    const endpoint = `${this.api}/add?pin=false`
+    // const endpoint = 'https://ipfs.infura.io:5001/api/v0/add?pin=false&wrap-with-directory=true'
+    // FIXME make File object
+    const data = await pkg.toBuffer()
+    const response = await request('POST', endpoint, {
       'Content-Type': 'multipart/form-data',
-      Body: await pkg.toBuffer()
+      Body: data
     })
     console.log('response', response.statusCode)
     const resp : any = await downloadStreamToBuffer(response)
-    console.log('hash: ', JSON.parse(resp.toString()).Hash)
+    // response is line-delimited json stream (see json stream)
+    const responses = resp.toString().split('\n').filter((l : string) => !!l).map((l: string) => JSON.parse(l))
+    /*
+    [
+      {
+        Name: 'foo.tar',
+        Hash: 'QmdRiUHrhzjqA6t47VvFngRxxmgG77ZHWmfppu3Zx35wo2',
+        Size: '3083'
+      }
+    ]
+    */
+    console.log('raw response', responses)
   }
 
 }
