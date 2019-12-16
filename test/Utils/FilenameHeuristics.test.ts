d5a7690d86c203f1d758fbbf595768331f704863
PhilippLgh <philipp@ethereum.org>
2019-12-16 16:50:15 +0100

    chore: add filename heuristic tests

---
+++ test/Utils/FilenameHeuristics.test.ts
@@ -0,0 +1,23 @@
+
+import { assert } from 'chai'
+import * as FilenameHeuristics from '../../src/utils/FilenameHeuristics'
+
+describe("FilenameHeuristics", () => {
+
+  describe('extractVersionFromString = (str : string | undefined) :string | undefined', () => {
+    const testCases = [
+      { fileName: 'foo.bla.tar.gz', expected: undefined },
+      { fileName: 'foo-1.0.0-bar.tar', expected: '1.0.0' },
+      { fileName: 'foo_1.0.0_bar', expected: '1.0.0' },
+      { fileName: 'geth-darwin-amd64-1.9.5-a1c09b93.tar.gz', expected: '1.9.5' },
+    ]
+    for (const testCase of testCases) {
+      const { fileName, expected } = testCase
+      it (`extracts the version info from ${fileName} -> ${expected}`, async () => {
+        const result = FilenameHeuristics.extractVersionFromString(fileName)
+        assert.equal(result, expected)
+      })
+    }
+  })
+
+})
