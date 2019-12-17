8dd8310c523100c6617ee52c29462ec3edfe850d
PhilippLgh <philipp@ethereum.org>
2019-12-17 18:24:14 +0100

    feat: add KeystoreUtils + tests

---
+++ src/PackageSigner/KeyStoreUtils.ts
@@ -0,0 +1,213 @@
+import crypto from 'crypto'
+import fs from 'fs'
+import path from 'path'
+import os from 'os'
+import { isDirSync } from '../util'
+const Wallet = require('ethereumjs-wallet')
+const secp256k1 = require('secp256k1')
+const asn1 = require('asn1.js')
+
+const SUPPORTED_KEYFILE_VERSIONS = [3]
+
+// https://github.com/ethereum/go-ethereum/wiki/Backup-&-restore#data-directory
+const getDefaultDataDir = () => {
+  switch (process.platform) {
+    case 'win32': return `${process.env.APPDATA}/Ethereum`
+    case 'linux': return '~/.ethereum'
+    case 'darwin': return '~/Library/Ethereum'
+    default: return '~/.ethereum'
+  }
+}
+
+export const getKeyStorePath = async () : Promise<string> => {
+  // TODO support different network IDs
+  const dataDir = getDefaultDataDir().replace('~', os.homedir())
+  const keystore = path.join(dataDir, 'keystore')
+  return keystore
+}
+
+export interface KeyFileInfo {
+  address: string;
+  fileName: string;
+  filePath: string;
+  type?: string; // experimental, non-standard : intended use case of the key
+}
+
+export const listKeys = async (keystorePath?: string) : Promise<Array<KeyFileInfo>> => {
+  const keystore = keystorePath || await getKeyStorePath()
+  // TODO we could filter keys here for e.g. a prefix like 'ethpkg' to avoid misuse
+  const keyFiles = fs.readdirSync(keystore).map(f => {
+    return {
+      // FIXME this is very fragile: parsing the address would be better but slower
+      address: '0x' + f.split('--').pop(),
+      fileName: f,
+      filePath: path.join(keystore, f)
+    }
+  })
+  const validKeyFiles = []
+  for (const key of keyFiles) {
+    const isValid = await isValidKeyStoreFile(key.filePath)
+    if (isValid) {
+      validKeyFiles.push(key)
+    }
+  }
+  return validKeyFiles
+}
+
+export const findKeyStoreFile = async (keyfilePathOrAlias: string, keyStorePath?: string) : Promise<string | undefined> => {
+  if (fs.existsSync(keyfilePathOrAlias)) {
+    return keyfilePathOrAlias
+  }
+  const keystore = keyStorePath || (await getKeyStorePath())
+
+  // try to expand
+  if (fs.existsSync(path.join(keystore, keyfilePathOrAlias))) {
+    return path.join(keystore, keyfilePathOrAlias)
+  }
+
+  // account referenced by address / alias
+  if (keyfilePathOrAlias.startsWith('0x')) {
+    const keys = await listKeys(keystore)
+    let key = keys.find(k => k.address.toLowerCase() === keyfilePathOrAlias.toLowerCase())
+    if (!key) {
+      return undefined
+    } 
+    return key.filePath
+  } else {
+    return undefined
+  }
+
+}
+
+export const isValidKeyStoreFile = async (keyfilePathOrAlias: string, keyStorePath?: string) : Promise<boolean> => {
+  const keyfilePath = await findKeyStoreFile(keyfilePathOrAlias, keyStorePath)
+  if (keyfilePath === undefined){ return false }
+  if (isDirSync(keyfilePath)) { return false }
+  const content = fs.readFileSync(keyfilePath, 'utf8')
+  let keyInfo
+  try {
+    keyInfo = JSON.parse(content)
+  } catch (error) {
+    // console.error('could not parse keyfile', keyfilePath, error)
+    // keyfile cannot be parsed
+    return false
+  }
+  return keyInfo && SUPPORTED_KEYFILE_VERSIONS.includes(keyInfo.version)
+}
+
+export const isValidPemKeyfile = async (keyfilePath: string) : Promise<boolean> => {
+  try {
+    let privateKey = await getPrivateKeyFromPEM(keyfilePath)
+    return privateKey !== undefined
+  } catch (error) {
+    // catch asn / der parser exceptions
+    return false
+  }
+}
+
+export const isKeyfile = async (keyfilePathOrAlias: string, keyStorePath?: string) : Promise<boolean> => {
+  const _isKeyStoreFile = await isValidKeyStoreFile(keyfilePathOrAlias, keyStorePath)
+  // perf: if already valid don't look further
+  if (_isKeyStoreFile) {
+    return true
+  }
+  const _isPemFile = await isValidPemKeyfile(keyfilePathOrAlias)
+  return _isPemFile
+}
+
+const getPrivateKeyFromPEM = async (keyfilePath: string) : Promise<Buffer | undefined> => {
+  const dearmor = (str: string) => {
+    /**
+     * handle
+    -----BEGIN EC PARAMETERS-----
+    BgUrgQQACg==
+    -----END EC PARAMETERS-----
+    -----BEGIN EC PRIVATE KEY-----
+    MHQCAQEEIGmUEA1rRoaDkaO5CN7ycmobPYXDc2djcRRrmq1IBZ7CoAcGBSuBBAAK
+    oUQDQgAE7FfkljPwW90lIHilxckicNZUDGACRDpnQCHth1+bUS7M50gqZAhkHfl+
+    js17MSsy5zE0VhxFTOiZiVhW+MZCPQ==
+    -----END EC PRIVATE KEY-----
+     */
+    const BEGIN_ARMOR = '-----BEGIN EC PRIVATE KEY-----'
+    str = str.substring(
+      str.lastIndexOf(BEGIN_ARMOR) + BEGIN_ARMOR.length, 
+      str.lastIndexOf("-----END EC PRIVATE KEY-----")
+    )
+    return str.split('\n').map(l => l.replace(/\s/g, "")).filter(l => !l.startsWith('-----')).join('')
+  }
+
+  if (!fs.existsSync(keyfilePath)) {
+    return undefined
+  }
+
+  const armoredKey = fs.readFileSync(keyfilePath, 'utf8')
+  const privKeyStr = dearmor(armoredKey)
+  const privKeyObjectDER = Buffer.from(privKeyStr, 'base64')
+
+  // https://tools.ietf.org/html/rfc5915
+  /*
+  ECPrivateKey ::= SEQUENCE {
+    version        INTEGER { ecPrivkeyVer1(1) } (ecPrivkeyVer1),
+    privateKey     OCTET STRING,
+    parameters [0] ECParameters {{ NamedCurve }} OPTIONAL,
+    publicKey  [1] BIT STRING OPTIONAL
+  }
+  */
+  const ECPrivateKey = asn1.define('ECPrivateKey', function (this: any) {
+    this.seq().obj(
+      this.key('version').int(),
+      this.key('privateKey').octstr(),
+      this.key('parameters').explicit(0).optional().objid(),
+      this.key('publicKey').explicit(1).optional().bitstr()
+    );
+  })
+
+  const result = ECPrivateKey.decode(privKeyObjectDER, 'der')
+  if (!result) {
+    // console.log('keyfile parser error')
+    return undefined
+  }
+  const { privateKey } = result
+
+  const verified = secp256k1.privateKeyVerify(privateKey)
+  if (!verified) {
+    // console.log('invalid private key')
+    return undefined
+  }
+
+  return privateKey
+}
+
+const getPrivateKeyFromKeystore = async (keyfilePath: string, password: string) : Promise<Buffer> => {
+  try {
+    const w = JSON.parse(fs.readFileSync(keyfilePath, 'utf8'))
+    const wallet = Wallet.fromV3(w, password)
+    const pk = wallet.getPrivateKey()
+    return pk
+  } catch (error) {
+    throw new Error('Key cannot be parsed')
+  }
+}
+
+export const getPrivateKey = async (keyfilePathOrAlias: string, passwordOrKeyStore?: string, password?: string) : Promise<Buffer> => {
+  const _isKeyStoreFile = await isValidKeyStoreFile(keyfilePathOrAlias, passwordOrKeyStore)
+  if (_isKeyStoreFile) {
+    const pw = password || passwordOrKeyStore
+    if (!pw) {
+      throw new Error('no password provided')
+    }
+    const keyfilePath = await findKeyStoreFile(keyfilePathOrAlias, passwordOrKeyStore)
+    return getPrivateKeyFromKeystore(<string>keyfilePath, pw)
+  }
+
+  const _isPemFile = await isValidPemKeyfile(keyfilePathOrAlias)
+  if (_isPemFile) {
+    const pk = await getPrivateKeyFromPEM(keyfilePathOrAlias)
+    if (pk === undefined) {
+      throw new Error('Private key could not be parsed from PEM file')
+    }
+    return pk
+  }
+
+  throw new Error('Keyfile not found or not a valid Ethereum or PEM file')
+}
---
+++ test/PackageSigner/KeyStoreUtils.test.ts
@@ -0,0 +1,145 @@
+import fs from 'fs'
+import path from 'path'
+import { assert } from 'chai'
+import PackageSigner from '../../src/PackageSigner'
+import { IPackage } from '../../src'
+import * as KeyStoreUtils from '../../src/PackageSigner/KeyStoreUtils'
+
+const KEYSTORE_PATH = path.join(__dirname, '..', 'fixtures', 'KeyStore')
+
+const KEY_1 = {
+  address: '0x585c34f863e4064bdeFA52305E3e7c89d39F98cF',
+  path: path.join(__dirname, '..', 'fixtures', 'KeyStore', 'UTC--2019-12-17T10-30-10.617174000Z--585c34f863e4064bdefa52305e3e7c89d39f98cf'),
+  fileName: 'UTC--2019-12-17T10-30-10.617174000Z--585c34f863e4064bdefa52305e3e7c89d39f98cf',
+  password: 'test',
+  privateKey: 'B9CB3C948D043BEF4551E1679D441F6DB567D0C74148166BFC6270D2E5D30E39'
+}
+
+const KEY_2 = {
+  address: '0x1cD1f547bE181Dcd53cF8Fb067A159f68938BfCd',
+  path: path.join(__dirname, '..', 'fixtures', 'KeyStore', 'UTC--2019-12-17T10-30-30.026422000Z--1cd1f547be181dcd53cf8fb067a159f68938bfcd'),
+  fileName: 'UTC--2019-12-17T10-30-30.026422000Z--1cd1f547be181dcd53cf8fb067a159f68938bfcd',
+  password: 'test2'
+}
+
+// http://openssl.cs.utah.edu/docs/apps/ec.html
+const PEM_KEY = {
+  // openssl ec -in ./test/fixtures/Keys/ec-codesign-pk.pem -text -noout
+  privateKey: '6994100d6b46868391a3b908def2726a1b3d85c373676371146b9aad48059ec2',
+  fileName: 'ec-codesign-pk.pem',
+  path: path.join(__dirname, '..', 'fixtures', 'Keys', 'ec-codesign-pk.pem')
+}
+
+const INVALID_KEYFILE_PATH = path.join(__dirname, '..', 'fixtures', 'KeyStore', 'INVALID_KEYFILE')
+const KEYFILE_1_PATH_OUTSIDE = path.join(__dirname, '..', 'fixtures', 'Keys', 'UTC--2019-12-17T10-30-10.617174000Z--585c34f863e4064bdefa52305e3e7c89d39f98cf')
+const KEYFILE_VERSION_1 = path.join(__dirname, '..', 'fixtures', 'Keys', 'UTC--2019-13-17T10-30-10.617174000Z--585c34f863e4064bdefa52305e3e7c89d39f98cf')
+
+describe("KeyStoreUtils", function() {
+
+  describe('getKeyStorePath = async () : Promise<string>', function() {
+    it('returns the path to the platform-specific default ethereum keystore whether it exists or not', async () => {
+      const keyStorePath = await KeyStoreUtils.getKeyStorePath()
+      assert.isDefined(keyStorePath)
+    })
+    it.skip('returns the path to the ethereum keystore of a certain network when passed a network identifier', async () => {
+      // TODO needs implementation
+    })
+  })
+
+  describe('listKeys = async (keystorePath?: string) : Promise<Array<KeyFileInfo>>', function() {
+    it('lists all keyfiles from ethereum keystore in KeyFileInfo format', async () => {
+      const keys = await KeyStoreUtils.listKeys(KEYSTORE_PATH)
+      const key1 = keys.find(k => k.address.toLowerCase() === KEY_1.address.toLowerCase())
+      assert.isDefined(key1)
+    })
+    it('filters invalid keyfiles from the list', async () => {
+      const keys = await KeyStoreUtils.listKeys(KEYSTORE_PATH)
+      assert.equal(keys.length, 2)
+    })
+  })
+
+  describe('findKeyStoreFile = async (keyfilePathOrAlias: string, keyStorePath?: string) : Promise<string | undefined>', function() {
+    it('finds the keyfile in a keystore given its address', async () => {
+      const keyFilePath = await KeyStoreUtils.findKeyStoreFile(KEY_1.address, KEYSTORE_PATH)
+      assert.equal(keyFilePath, KEY_1.path)
+    })
+    it('expands a relative file path or name to an absolute path', async () => {
+      const keyFilePath = await KeyStoreUtils.findKeyStoreFile(KEY_1.fileName, KEYSTORE_PATH)
+      assert.equal(keyFilePath, KEY_1.path)
+    })
+  })
+
+  describe('isValidKeyStoreFile = async (keyfilePathOrAlias: string, keyStorePath?: string) : Promise<boolean>', function() {
+    it('returns true if the file path exists in the keystore and is a valid keyfile', async () => {
+      const result = await KeyStoreUtils.isValidKeyStoreFile(KEY_1.path, KEYSTORE_PATH)
+      assert.isTrue(result)
+    })
+    it('returns true if the file exists outside the keystore but is a valid keyfile', async () => {
+      const result = await KeyStoreUtils.isValidKeyStoreFile(KEYFILE_1_PATH_OUTSIDE)
+      assert.isTrue(result)
+    })
+    it('returns true if the provided alias / eth address can be resolved to a valid file in the keystore', async () => {
+      const result = await KeyStoreUtils.isValidKeyStoreFile(KEY_1.address, KEYSTORE_PATH)
+      assert.isTrue(result)
+    })
+    it('returns false if the file exists but has an unsupported version', async () => {
+      const result = await KeyStoreUtils.isValidKeyStoreFile(KEYFILE_VERSION_1)
+      assert.isFalse(result)
+    })
+    it('returns false if the file exists but is an invalid or no keyfile', async () => {
+      const result = await KeyStoreUtils.isValidKeyStoreFile(INVALID_KEYFILE_PATH)
+      assert.isFalse(result)
+    })
+  })
+
+  describe('isValidPemKeyfile = async (keyfilePath: string) : Promise<boolean> ', function() {
+    it('returns true if the file contains a PEM encoded secp256k1 private key', async () => {
+      const result = await KeyStoreUtils.isValidPemKeyfile(PEM_KEY.path)
+      assert.isTrue(result)
+    })
+    it.skip('returns false if the file contains a PEM encoded private key that is not secp256k1', async () => {
+      // TODO needs test case
+    })
+    it('returns false if the file is not a PEM file', async () => {
+      const result = await KeyStoreUtils.isValidPemKeyfile(INVALID_KEYFILE_PATH)
+      assert.isFalse(result)
+    })
+  })
+
+  describe('isKeyfile = async (keyfilePathOrAlias: string, keyStorePath?: string) : Promise<boolean>', function() {
+    it('returns true if the provided path points to a valid ethereum keyfile', async () => {
+      const result = await KeyStoreUtils.isKeyfile(KEY_1.path)
+      assert.isTrue(result)
+    })
+    it('returns false if the provided path points to an invalid ethereum keyfile', async () => {
+      const result = await KeyStoreUtils.isKeyfile(KEYFILE_VERSION_1)
+      assert.isFalse(result)
+    })
+    it('returns true if the provided path points to a valid PEM keyfile', async () => {
+      const result = await KeyStoreUtils.isKeyfile(PEM_KEY.path)
+      assert.isTrue(result)
+    })
+    it('returns false if the provided path points to a non-keyfile-file', async () => {
+      const result = await KeyStoreUtils.isKeyfile(INVALID_KEYFILE_PATH)
+      assert.isFalse(result)
+    })
+    it('returns false if the provided path does not exist', async () => {
+      const result = await KeyStoreUtils.isKeyfile(path.join(__dirname, '123456789'))
+      assert.isFalse(result)
+    })
+  })
+
+  describe('getPrivateKey = async (keyfilePathOrAlias: string, password?: string) : Promise<Buffer>', function() {
+    this.timeout(30*1000)
+    it('decrypts the keystore file using <password> and returns the private key as buffer', async () => {
+      const result = await KeyStoreUtils.getPrivateKey(KEY_1.path, KEY_1.password)
+      assert.equal(result.toString('hex').toLowerCase(), KEY_1.privateKey.toLowerCase())
+    })
+    it('detects and decodes PEM files and returns private key as buffer', async () => {
+      const result = await KeyStoreUtils.getPrivateKey(PEM_KEY.path)
+      assert.equal(result.toString('hex').toLowerCase(), PEM_KEY.privateKey.toLowerCase())
+    })
+  })
+
+
+})
