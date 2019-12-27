0fe61b6afe72bfca6d205c4732b283804f694131
PhilippLgh <philipp@ethereum.org>
2019-12-27 16:48:49 +0100

    chore: improve interactive user file path prompt

--- src/bin/lib/InputFilepath.ts
+++ src/bin/lib/InputFilepath.ts
@@ -2,8 +2,20 @@ import * as fs from 'fs'
 import path from 'path'
 
 import { prompt } from 'enquirer'
+import { hasPackageExtension } from '../../utils/FilenameUtils'
 
-export const getUserFilePath = async (message: string) => {
+const isValid = (filePath?: string) => filePath && fs.existsSync(filePath) && hasPackageExtension(filePath)
+
+export const getUserFilePath = async (message: string, filePath?: string) : Promise<string | undefined>  => {
+  if (isValid(filePath)) {
+    return filePath
+  }
+  if (filePath && !path.isAbsolute(filePath)) {
+    filePath = path.join(process.cwd(), filePath)
+    if (isValid(filePath)) {
+      return filePath
+    }
+  }
   const questionFile = (message: string) => [{
     type: 'input',
     name: 'file',
@@ -11,9 +23,8 @@ export const getUserFilePath = async (message: string) => {
     initial: ''
   }];
   let { file } = await prompt(questionFile(message))
-  if (!file || !fs.existsSync(file)) {
-    console.log(`>> file not found: "${file}"`)
-    return ''
+  if (isValid(file)) {
+    return file
   }
   return file
 }
