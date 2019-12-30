a2e6c8087bff63356ba8a8dc350cb0f4013aa7da
PhilippLgh <philipp@ethereum.org>
2019-12-30 17:36:10 +0100

    chore: remove interface file

--- src/IJWS.ts
+++
@@ -1,6 +0,0 @@
-export interface IFlattenedJwsSerialization {
-  header?: any,
-  protected?: string, // base64url
-  payload: string | any, // base64url | json object
-  signature: string // base64url
-}
