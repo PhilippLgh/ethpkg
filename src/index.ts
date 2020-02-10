bd2b1202393232074b5fb92b304052573a448893
PhilippLgh <philipp@ethereum.org>
2020-02-10 16:01:20 +0100

    fix: export important interfaces and enums

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,7 @@ export { PackageManager }
 
 export default new PackageManager()
 
+export { PROCESS_STATES } from './IStateListener'
 export * from './PackageManager/IPackage'
+export * from './Repositories/IRepository'
+
