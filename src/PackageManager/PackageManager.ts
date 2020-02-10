4c706379f6e9a63aa30ac195e9f15a1cc8a0c697
PhilippLgh <philipp@ethereum.org>
2020-02-10 15:57:43 +0100

    fix: various interface and cache issues

--- src/PackageManager/PackageManager.ts
+++ src/PackageManager/PackageManager.ts
@@ -4,19 +4,19 @@ import { IPackage, instanceofIPackage } from './IPackage'
 
 import Fetcher from '../Fetcher'
 import * as PackageSigner from '../PackageSigner'
-import { StateListener } from '../IStateListener'
-import { IRelease, FetchOptions, IRepository } from '../Repositories/IRepository'
+import { IRelease, FetchOptions, IRepository, instanceOfIRelease } from '../Repositories/IRepository'
 import { ResolvePackageOptions, instanceofResolvePackageOptions, PackageQuery, instanceOfPackageQuery, DownloadPackageOptions } from '../Fetcher/Fetcher'
 import getRepository from '../Repositories/RepositoryManager'
 import { IVerificationResult } from '../IVerificationResult'
 import ISigner from '../PackageSigner/ISigner'
-import { getPackage, PackageData, instanceOfPackageData } from './PackageService'
-import { withCache, MemCache, PersistentJsonCache, ICache } from './Cache'
+import { toPackage, PackageData, instanceOfPackageData } from './PackageService'
+import { withCache, MemCache, PersistentJsonCache, ICache, NoCache, instanceOfICache } from './Cache'
 import { SerializationInfo, ISerializable } from './ISerializable'
 import TarPackage from './TarPackage'
 import ZipPackage from './ZipPackage'
 import { isDirSync, ConstructorOf } from '../util'
 import RepositoryManager from '../Repositories/RepositoryManager'
+import { isArray } from 'util'
 
 // browser / webpack support
 if (!fs.existsSync) {
@@ -24,18 +24,29 @@ if (!fs.existsSync) {
 }
 
 // we need to tell th ecahe how to restore persisted objects
-const packageFactory = (info: SerializationInfo) : Promise<IPackage | undefined> => {
+const packageFactory = async (info: SerializationInfo) : Promise<IPackage | undefined> => {
   const { ctor, data } = info
-  const { filePath, buffer } = data
-  // FIXME restore fileName
   if (ctor === undefined) {
-    return Promise.resolve(undefined)
+    return undefined
   }
+  if (!data) {
+    return data
+  }
+  if(ctor === 'Object') {
+    return data
+  }
+  // FIXME restore fileName
   else if (ctor === 'ZipPackage') {
-    return new ZipPackage(filePath).loadBuffer(buffer)
+    const { filePath, buffer, metadata } = data
+    const pkg = await new ZipPackage(filePath).loadBuffer(buffer)
+    pkg.metadata = metadata
+    return pkg
   } 
   else if(ctor === 'TarPackage') {
-    return new TarPackage(filePath).loadBuffer(buffer)
+    const { filePath, buffer, metadata } = data
+    const pkg = await new TarPackage(filePath).loadBuffer(buffer)
+    pkg.metadata = metadata
+    return pkg
   }
   else {
     throw new Error('De-serialization error: unknown ctor'+ctor)
@@ -46,25 +57,41 @@ export interface PackOptions {
   type?: string;
 }
 
+export interface PackageManagerOptions {
+  cache?: string | ICache<any>
+}
+
 export default class PackageManager {
 
-  private cache?: ICache<ISerializable>;
+  private cache: ICache<ISerializable> = new NoCache()
 
   private repoManager: getRepository
 
   private signers: Array<ISigner> = []
 
-  constructor(options?: any) {
+  // FIXME type options
+  constructor(options?: PackageManagerOptions) {
 
     this.repoManager = new RepositoryManager()
 
+    let cacheInit = false
     if (options && options.cache) { 
-      if (isDirSync(options.cache)) {
+      if(instanceOfICache(options.cache)) {
+        this.cache = options.cache
+        cacheInit = true
+      }
+      else if(isDirSync(options.cache)) {
         this.cache = new PersistentJsonCache(options.cache, packageFactory)
-        this.getPackage = withCache(this.cache, this.getPackage)
-      } else {
+        cacheInit = true
+      } 
+      else {
         throw new Error('Invalid cache path provided: not accessible -'+options.cache)
       }
+    }
+
+    if (cacheInit) {
+      this.resolve = withCache(this.cache, this.resolve.bind(this), (spec: PackageQuery) => `resolve:${spec}`)
+      this.getPackage = withCache(this.cache, this.getPackage.bind(this))
     }
     
   }
@@ -78,7 +105,7 @@ export default class PackageManager {
   }
 
   async getRepository(name: string, options: any) : Promise<IRepository | undefined> {
-    return this.repoManager.getRepository(name, options)
+    return this.repoManager.getRepository(options)
   }
 
   async listRepositories() : Promise<Array<string>> {
@@ -119,6 +146,13 @@ export default class PackageManager {
   async listPackages(spec: PackageQuery, options?: FetchOptions) : Promise<Array<IRelease>> {
     const fetcher = new Fetcher(this.repoManager)
     const releases = await fetcher.listReleases(spec, options)
+    if (options && options.cache) {
+      if (typeof options.cache === 'string') {
+        // FIXME fs.existsSync(options.cache)
+      } else if (isArray(options.cache)) {
+
+      }
+    }
     return releases
   }
 
@@ -128,43 +162,62 @@ export default class PackageManager {
     return release
   }
 
-  async fetchPackage(release: IRelease, options?: DownloadPackageOptions) : Promise<IPackage | undefined> {
-    const fetcher = new Fetcher(this.repoManager)
-    const buf = await fetcher.downloadPackage(release, options)
-    const pkg = await getPackage(buf, release)
-    return pkg
-  }
-
   /**
    * Downloads a package to disk
    * A combination of resolve, fetchPackage and verify. Steps can be specified through download options
    */
-  async downloadPackage(pkgSpec: PackageQuery, options?: DownloadPackageOptions) : Promise<IPackage> {
-    const release = await this.resolve(pkgSpec, options)
-    if (!release) {
-      throw new Error(`Package query "${pkgSpec}" could not be resolved`)
-    }
-    const pkg = await this.fetchPackage(release, options)
-    if (!pkg) {
-      throw new Error('Package could not be fetched')
-    }
+  private async downloadPackage(release: IRelease, options?: DownloadPackageOptions) : Promise<IPackage> {
+
+    options = Object.assign({}, options)
+
+    const fetcher = new Fetcher(this.repoManager)
+    const buf = await fetcher.downloadPackage(release, options)
+    const pkg = await toPackage(buf, release) // != this.getPackage
 
     // TODO if download options verify
-    /*
-    let addressOrEnsName = undefined
-    if (verifyWith.length > 0) {
-      let author = verifyWith[0]
-      if (typeof author === 'string') {
-        addressOrEnsName = author
-      } else if ('address' in author) {
-        addressOrEnsName = author.address
+    if (options.verify) {
+      /*
+      let addressOrEnsName = undefined
+      if (verifyWith.length > 0) {
+        let author = verifyWith[0]
+        if (typeof author === 'string') {
+          addressOrEnsName = author
+        } else if ('address' in author) {
+          addressOrEnsName = author.address
+        }
       }
+      */
     }
-    */
 
-    if (options && options.destPath) {
+    const isDirPath = (str:string) => !path.extname(str)
+    if (options.destPath) {
       let destPath = path.resolve(options.destPath)
+      console.log('is dir?', path.extname(destPath), destPath, isDirPath(destPath))
+      if (isDirPath(destPath)) {
+        if(isDirSync(destPath)) {
+          destPath = path.join(destPath, release.fileName)
+        } else {
+          // TODO try create dir if non-existent dir path
+          fs.mkdirSync(destPath, {
+            recursive: true
+          })
+        }
+      }
+      console.log('write package to disk', destPath, release.fileName)
       await pkg.writePackage(destPath)
+      if(pkg.metadata) {
+        pkg.metadata.remote = false // indicate that local version is available
+      }
+      console.log('package written to', destPath)
+    }
+
+    if (options.extract) {
+      if (options.destPath && isDirSync(options.destPath)) {
+        await pkg.extract(options.destPath, () => {
+
+        })
+        // TODO stateListener(PROCESS_STATES.EXTRACT_FINISHED, { location, size: packageData.length, release })
+      }
     }
 
     return pkg
@@ -172,17 +225,16 @@ export default class PackageManager {
 
   /**
    * Creates and returns an IPackage based on a filepath, url, or package specifier
-   * similar to downloadPackage (called internally) but allows to specify the package in more ways and should be default method
    */
-  async getPackage(pkgSpec: PackageQuery | PackageData | ResolvePackageOptions, options? : ResolvePackageOptions) : Promise<IPackage | undefined> {
+  async getPackage(pkgSpec: IRelease | PackageData | PackageQuery | ResolvePackageOptions, options? : ResolvePackageOptions) : Promise<IPackage | undefined> {
     
     if (!pkgSpec) {
       throw new Error('Invalid package specification: empty or undefined')
-    }
+    } 
 
     // pkgSpec is already available as buffer, File (browser), IPackage or file path => no fetch
     if (instanceOfPackageData(pkgSpec )) {
-      return getPackage(pkgSpec)
+      return toPackage(pkgSpec)
     }
 
     // check if the short-hand one argument form is used and extract <PackageQuery>pkgSpec from options before we try to resolve them
@@ -197,11 +249,14 @@ export default class PackageManager {
       pkgSpec = pkgSpec.spec 
     }
 
-    // try to resolve package queries
+    // try to resolve package queries to IRelease
     if (instanceOfPackageQuery(pkgSpec)) {
       try {
-        const pkg = await this.downloadPackage(pkgSpec, options)
-        return pkg
+        const release = await this.resolve(pkgSpec, options)
+        if (!release) {
+          throw new Error(`Package query "${pkgSpec}" could not be resolved`)
+        }
+        pkgSpec = release
       } catch (error) {
         // TODO log error here
         // console.log('error during download', error)
@@ -209,17 +264,25 @@ export default class PackageManager {
       }
     }
 
-    // TODO handle caching
-    /*
-    if (options && options.cache && fs.existsSync(options.cache)) {
-      let cachedData = path.join(options.cache, release.fileName)
-      if (fs.existsSync(cachedData)) {
-        const pkg = await getPackageFromFile(cachedData)
-        pkg.metadata = release
-        return pkg
+    // download IRelease if it does not exist in cache
+    if (instanceOfIRelease(pkgSpec)) {
+
+      const release : IRelease = pkgSpec
+
+      // TODO handle caching
+      // TODO write tests
+      if (options && options.cache && fs.existsSync(options.cache)) {
+        let cachedData = path.join(options.cache, release.fileName)
+        if (fs.existsSync(cachedData)) {
+          const pkg = await toPackage(cachedData)
+          pkg.metadata = release
+          return pkg
+        }
       }
-    } 
-    */
+
+      return this.downloadPackage(release, options)
+    }
+
     throw new Error('unsupported input type for package')
   }
 
