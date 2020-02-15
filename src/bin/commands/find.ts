0a45c6fb7e522ad615e8577fb452b940605689c9
PhilippLgh <philipp@ethereum.org>
2020-02-15 19:45:59 +0100

    chore: use print utils in commands

--- src/bin/commands/find.ts
+++ src/bin/commands/find.ts
@@ -1,21 +1,29 @@
 import { Command, command, param } from 'clime'
 import PackageManager from '../../PackageManager/PackageManager'
-import boxen from 'boxen'
+import { printFormattedRelease, createCLIPrinter } from '../printUtils'
 
 @command({
-  description: 'finds a package and display release info',
+  description: 'Finds a package and display release info',
 })
 export default class extends Command {
   public async execute(
     @param({
-      name: 'specifier',
-      description: 'package specifier',
+      name: 'query',
+      description: 'package query',
       required: true,
     })
     spec: string
   ) {
     const packageManager = new PackageManager()
-    const release = await packageManager.findPackage(spec)
-    console.log(boxen(JSON.stringify(release, undefined, 2)))
+    let release
+    const printer = createCLIPrinter()
+    try {
+      release = await packageManager.resolve(spec, {
+        listener: printer.listener
+      })
+    } catch (error) {
+      return printer.fail(error)
+    }
+    printFormattedRelease(release)
   }
 }
--- src/bin/commands/list.ts
+++ src/bin/commands/list.ts
@@ -1,9 +1,6 @@
 import { Command, command, param, Options, option } from 'clime'
 import PackageManager from '../../PackageManager/PackageManager'
-import chalk from 'chalk';
-import { printFormattedReleaseList } from '../utils'
-import { PROCESS_STATES } from '../../IStateListener'
-import ora = require('ora')
+import { printError, printFormattedReleaseList, createCLIPrinter, PROCESSES } from '../printUtils'
 
 export class ListOptions extends Options {
   @option({
@@ -31,42 +28,15 @@ export default class extends Command {
     // console.log('attributes', attributes)
     const packageManager = new PackageManager()
     let releases = []
+    const printer = createCLIPrinter([PROCESSES.FETCHING_RELEASE_LIST, PROCESSES.FILTER_RELEASE_LIST])
     try {
-      let task : undefined | ora.Ora
       releases = await packageManager.listPackages(spec, {
         limit: 50,
         prefix: undefined,
-        listener: (newState: string, args: any = {}) => {
-          switch(newState) {
-            case PROCESS_STATES.FETCHING_RELEASE_LIST_STARTED: {
-              const { repo } = args
-              task = ora(`Fetching releases from ${repo}`).start()
-              break;
-            }
-            case PROCESS_STATES.FETCHING_RELEASE_LIST_FINISHED: {
-              const { releases, repo } = args
-              if (task !== undefined) {
-                task.succeed(`Fetched ${releases.length} releases from ${repo}`)
-              }
-              break;
-            }
-            case PROCESS_STATES.FILTER_RELEASE_LIST_STARTED: {
-              task = ora(`Filtering releases`).start()
-              break;
-            }
-            case PROCESS_STATES.FILTER_RELEASE_LIST_FINISHED: {
-              const { releases } = args
-              if (task !== undefined) {
-                task.succeed(`Filtered releases to ${releases.length}`)
-              }
-              break;
-            }
-          }
-          // console.log('new state', newState, Object.keys(args))
-        }
+        listener: printer.listener
       })
     } catch (error) {
-      return console.log(chalk.red(error.message))
+      return printer.fail(error)
     }
     printFormattedReleaseList(releases, attributes)
   }
