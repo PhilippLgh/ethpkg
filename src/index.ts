e735d35cf12bd9337e0a3d180bcf112c2c9941f6
PhilippLgh <philipp@ethereum.org>
2020-02-17 17:23:39 +0100

    chore: export important helpers

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,6 @@ export default new PackageManager()
 export { PROCESS_STATES } from './IStateListener'
 export * from './PackageManager/IPackage'
 export * from './Repositories/IRepository'
-
+export { instanceOfPackageQuery } from './Fetcher/Fetcher'
+export { default as ISigner } from './PackageSigner/ISigner'
+export { default as KeyStore } from './PackageSigner/KeyStore'
