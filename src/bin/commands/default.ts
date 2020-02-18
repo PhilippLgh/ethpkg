22ec322d3822bf34069e45728be0872fed1bbbea
PhilippLgh <philipp@ethereum.org>
2020-02-18 11:24:21 +0100

    chore: cleanup command descriptions

--- src/bin/commands/default.ts
+++ src/bin/commands/default.ts
@@ -3,26 +3,10 @@ import {SubcommandDefinition} from 'clime'
 export const subcommands: SubcommandDefinition[] = [
   {
     name: 'key',
-    brief: 'generate and manage keys',
+    brief: 'Generate and manage keys',
   },
   {
     name: 'cert',
     brief: 'create certificates',
-  },
-  {
-    name: 'pack',
-    brief: 'create unsigned packages from dir',
-  },
-  {
-    name: 'sign',
-    brief: 'sign packages',
-  },
-  {
-    name: 'verify',
-    brief: 'verify packages',
-  },
-  {
-    name: 'version',
-    brief: 'print version number',
-  },
+  }
 ];
