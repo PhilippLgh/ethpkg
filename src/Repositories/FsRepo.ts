7af33a87cae2f74287325222b964e82d67d722d8
PhilippLgh <philipp@ethereum.org>
2020-02-29 15:29:40 +0100

    chore: improve fsrepo to use it for caching

--- src/Repositories/FsRepo.ts
+++ src/Repositories/FsRepo.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import path from 'path'
 import { IRepository, FetchOptions, IRelease } from './IRepository'
 
 export default class FileSystemRepository implements IRepository {
@@ -9,14 +10,45 @@ export default class FileSystemRepository implements IRepository {
   }) {
     this.dirPath = project
   }
+  private toRelease(fileName: string) : IRelease {
+    const fullPath = path.join(this.dirPath, fileName)
+    try {
+      const data = JSON.parse(fs.readFileSync(fullPath, 'utf8'))
+      const {
+        name, version, displayVersion, channel, fileName,
+        updated_ts, updated_at, size, original, error, location, remote
+      } = data
+      return {
+        name,
+        version,
+        displayVersion, // v prefixed short version without prerelease info
+        channel,
+        fileName,
+        updated_ts, // timestamp: last date of modification / (re-) publishing
+        updated_at, // display date version
+        size, // size of the asset / package
+        original, // the original response object before it was parsed
+        error, // error message
+        location, // download url or path to package
+        remote // if package is available locally or only remote
+      }
+    } catch (error) {
+      return {
+        fileName,
+        error: 'Could not parse metadata file'
+      }
+    }
+  }
+  /**
+   * The FS Repo expects a structure:
+   * my-package-1.0.0.tar.gz
+   * my-package-1.0.0.tar.gz.json
+   * where package and metadata are stored next to each other
+   * @param options 
+   */
   async listReleases(options?: FetchOptions | undefined): Promise<Array<IRelease>> {
     const files = fs.readdirSync(this.dirPath)
-    const releases = files.map(f => {
-      const release : IRelease = {
-        fileName: f
-      }
-      return release
-    })
+    const releases = files.filter(f => f.endsWith('.json')).map(this.toRelease.bind(this))
     return releases
   }
 }
