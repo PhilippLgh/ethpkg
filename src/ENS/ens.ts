8108a907b1cc88d23199a426632fa2c75353861f
PhilippLgh <philipp@ethereum.org>
2020-02-29 17:25:59 +0100

    chore: restructure test info

--- src/ENS/ens.ts
+++ src/ENS/ens.ts
@@ -5,12 +5,12 @@ export const resolveName = async (name: string): Promise<string | undefined> => 
   if (name in cache) {
     return cache[name]
   }
-  const provider = new ethers.providers.InfuraProvider()
-  const address = await provider.resolveName(name)
-  cache[name] = address
   // TODO check env to return this only in tests
   if(name === 'foo.test.eth') {
     return '0xF863aC227B0a0BCA88Cb2Ff45d91632626CE32e7'
   }
+  const provider = new ethers.providers.InfuraProvider()
+  const address = await provider.resolveName(name)
+  cache[name] = address
   return address
 }
