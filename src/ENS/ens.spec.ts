ba8e74ed604d9a39886653e4891da233a55085bd
PhilippLgh <philipp@ethereum.org>
2020-04-20 11:42:20 +0200

    feat: add ens test for grid.philipplgh.eth

--- src/ENS/ens.spec.ts
+++ src/ENS/ens.spec.ts
@@ -11,6 +11,10 @@ describe('ENS', () => {
       const address = await resolveName('ethpkg.eth')
       assert.equal(address, '0x6efeF34e81FD201EdF18C7902948168E9eBb88aE')
     })
+    it('resolves grid.philipplgh.eth ', async () => {
+      const address = await resolveName('grid.philipplgh.eth')
+      assert.equal(address?.toLowerCase(), '0x39830fed4b4b17fcdfa0830f9ab9ed8a1d0c11d9')
+    })
   })
   describe('lookup', () => {
     
